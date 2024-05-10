0e72a64f0f0abd5446af878a01e5fd757a3bd9f5
Sharad20-02 <sharadkatkol596@gmail.com>
2024-05-10 06:26:24 +0530

    commit

---
+++ PLaybackResumption.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class PlaybackHistory {
+    private Map<String, Integer> playbackPositions;
+
+    public PlaybackHistory() {
+        playbackPositions = new HashMap<>();
+    }
+
+    public void savePlaybackPosition(String userId, int position) {
+        playbackPositions.put(userId, position);
+        System.out.println("Playback position saved for user " + userId + ": " + position);
+    }
+
+    public int getPlaybackPosition(String userId) {
+        return playbackPositions.getOrDefault(userId, 0);
+    }
+}
+
+public class PLaybackResumption {
+    public static void main(String[] args) {
+
+        String userId = "user123";
+
+        int playbackPosition = 120;
+
+        PlaybackHistory playbackHistory = new PlaybackHistory();
+
+        playbackHistory.savePlaybackPosition(userId, playbackPosition);
+
+        int resumedPlaybackPosition = playbackHistory.getPlaybackPosition(userId);
+        System.out.println("Resumed playback position for user " + userId + ": " + resumedPlaybackPosition);
+    }
+}
